0a1b08ac8cee501ac9cf861108413d682f33cd2c
Taylordglaser <62516890+Taylordglaser@users.noreply.github.com>
2021-12-27 17:20:38 -0600

    Add files via upload

---
+++ src/components/NewsFeed.js
@@ -0,0 +1,39 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const NewsFeed = () => {
+  const [articles, setArticles]= useState(null)
+
+  useEffect(() => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://crypto-news-live.p.rapidapi.com/news',
+      headers: {
+        'x-rapidapi-host': 'crypto-news-live.p.rapidapi.com',
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
+      }
+    }
+    
+    axios.request(options).then((response) => {
+      console.log(response.data)
+      setArticles(response.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  }, [])
+
+    const first9Articles = articles?.slice(0,9)
+
+    return (
+      <div className="news-feed">
+          <h2>News Feed</h2>
+          {first9Articles?.map((article, _index) => (
+              <div key={_index}>
+                  <a href={article.url}><p>{article.title}</p></a>
+              </div>))}
+      </div>
+    )
+  }
+  
+  export default NewsFeed
